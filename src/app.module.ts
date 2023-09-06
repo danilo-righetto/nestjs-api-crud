fee817a22c6a2d09ddf97e802fd278e4f5e1bf11
Danilo Righetto <drighetto@fcl.com.br>
2023-09-06 14:36:51 -0300

    Ajustando a classe de controle

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://danilonewtrue:<password>@cluster0.jdx11yv.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
